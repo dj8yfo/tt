9c9cbb6acb08811697739fd6ca0078c079ada6fb
dj8yf0μl <sffhox@gmail.com>
2022-09-28 21:52:08 +0300

    checkpoints tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
 pub mod trivial;
+pub trait Mappy<K, V: Clone> {
+    fn insert(&mut self, k: K, v: V) -> Option<V>;
+    fn get(&self, k: &K) -> Option<&V>;
+    fn remove(&mut self, k: &K) -> Option<V>;
+
+    fn checkpoint(&mut self, tag: String);
+    fn rollback(&mut self, tag: String) -> bool;
+    fn prune(&mut self);
+}
--- src/trivial/mod.rs
+++ src/trivial/mod.rs
@@ -16,16 +16,6 @@ pub struct VMapTriv<K, V> {
     inner: HashMap<Version, HashMap<K, V>>,
 }
 
-pub trait Mappy<K, V: Clone> {
-    fn insert(&mut self, k: K, v: V) -> Option<V>;
-    fn get(&self, k: &K) -> Option<&V>;
-    fn remove(&mut self, k: &K) -> Option<V>;
-
-    fn checkpoint(&mut self, tag: String);
-    fn rollback(&mut self, tag: String) -> bool;
-    fn prune(&mut self);
-}
-
 impl<K, V> VMapTriv<K, V> {
     #[allow(clippy::new_without_default)]
     pub fn new() -> Self {
@@ -35,7 +25,7 @@ impl<K, V> VMapTriv<K, V> {
     }
 }
 
-impl<K, V> Mappy<K, V> for VMapTriv<K, V>
+impl<K, V> super::Mappy<K, V> for VMapTriv<K, V>
 where
     K: Eq + Hash + Clone,
     V: Clone,
@@ -131,29 +121,185 @@ mod tests {
             .collect();
         product
     }
+
     #[test]
-    fn get_what_you_give_continuous() {
+    fn get_inserted() {
         fn property(keys: HashSet<String>) -> TestResult {
             let entries = cartesian_product(keys);
-            let entries_to_remove = entries.clone();
 
             let hashmap = VMapTriv::new();
-            let mut system_under_test: Box<dyn Mappy<String, u32>> = Box::new(hashmap);
-            for entry in entries.into_iter() {
-                let (key, value) = entry;
-                let k_clone = key.clone();
+            let mut under_test: Box<dyn crate::Mappy<String, u32>> = Box::new(hashmap);
 
-                system_under_test.insert(key, value);
-                assert_eq!(Some(&value), system_under_test.get(&k_clone));
+            for (key, value) in entries.clone() {
+                assert_eq!(None, under_test.get(&key));
+                under_test.insert(key, value);
+            }
+            for (key, value) in &entries {
+                assert_eq!(Some(value), under_test.get(key));
             }
 
-            for entry in entries_to_remove.into_iter() {
-                let (key, value) = entry;
+            TestResult::passed()
+        }
+        // quickcheck doesn't work with closures, unfortunately
+        QuickCheck::new().quickcheck(property as fn(HashSet<String>) -> TestResult);
+    }
 
-                let val_removed = system_under_test.remove(&key);
+    #[test]
+    fn remove_inserted() {
+        fn property(keys: HashSet<String>) -> TestResult {
+            let entries = cartesian_product(keys);
+
+            let hashmap = VMapTriv::new();
+            let mut under_test: Box<dyn crate::Mappy<String, u32>> = Box::new(hashmap);
+
+            for (key, value) in entries.clone() {
+                under_test.insert(key, value);
+            }
+
+            for (key, value) in entries {
+                let val_removed = under_test.remove(&key);
                 assert_eq!(Some(value), val_removed);
-                assert_eq!(None, system_under_test.get(&key));
+                assert_eq!(None, under_test.get(&key));
             }
+
+            TestResult::passed()
+        }
+        // quickcheck doesn't work with closures, unfortunately
+        QuickCheck::new().quickcheck(property as fn(HashSet<String>) -> TestResult);
+    }
+
+    #[test]
+    fn checkpoint_rollback_union() {
+        fn property(keys_one: HashSet<String>, keys_two: HashSet<String>) -> TestResult {
+            let keys_one = keys_one.difference(&keys_two).map(Clone::clone).collect();
+
+            let epochs: Vec<Vec<(String, u32)>> = [keys_one, keys_two]
+                .into_iter()
+                .map(cartesian_product)
+                .collect();
+
+            let hashmap = VMapTriv::new();
+            let mut under_test: Box<dyn crate::Mappy<String, u32>> = Box::new(hashmap);
+
+            under_test.checkpoint("EMPTY".to_owned());
+
+            for (key, value) in epochs[0].clone() {
+                under_test.insert(key, value);
+            }
+
+            under_test.checkpoint("ONE".to_owned());
+
+            for (key, value) in epochs[1].clone() {
+                under_test.insert(key, value);
+            }
+
+            under_test.checkpoint("TWO".to_owned());
+
+            for entries in &epochs {
+                for (k, v) in entries {
+                    assert_eq!(Some(v), under_test.get(k))
+                }
+            }
+            assert!(under_test.rollback("EMPTY".to_owned()));
+            for entries in &epochs {
+                for (k, _v) in entries {
+                    assert_eq!(None, under_test.get(k))
+                }
+            }
+            assert!(under_test.rollback("ONE".to_owned()));
+            for (k, v) in &epochs[0] {
+                assert_eq!(Some(v), under_test.get(k))
+            }
+            for (k, _v) in &epochs[1] {
+                assert_eq!(None, under_test.get(k))
+            }
+
+            assert!(under_test.rollback("TWO".to_owned()));
+            for entries in &epochs {
+                for (k, v) in entries {
+                    assert_eq!(Some(v), under_test.get(k))
+                }
+            }
+
+            TestResult::passed()
+        }
+        // quickcheck doesn't work with closures, unfortunately
+        QuickCheck::new()
+            .quickcheck(property as fn(HashSet<String>, HashSet<String>) -> TestResult);
+    }
+
+    #[test]
+    fn checkpoint_rollback_difference() {
+        fn property(
+            keys_left: HashSet<String>,
+            keys_deleted: HashSet<String>,
+        ) -> TestResult {
+            let keys_left: HashSet<String> = keys_left
+                .difference(&keys_deleted)
+                .map(Clone::clone)
+                .collect();
+            let keys_full = keys_left.union(&keys_deleted).map(Clone::clone).collect();
+
+            let epochs: Vec<Vec<(String, u32)>> = [keys_full, keys_deleted]
+                .into_iter()
+                .map(cartesian_product)
+                .collect();
+
+            let hashmap = VMapTriv::new();
+            let mut under_test: Box<dyn crate::Mappy<String, u32>> = Box::new(hashmap);
+
+            for (key, value) in epochs[0].clone().into_iter() {
+                under_test.insert(key, value);
+            }
+
+            under_test.checkpoint("ONE".to_owned());
+
+            for (key, _v) in &epochs[1] {
+                under_test.remove(key);
+            }
+
+            under_test.checkpoint("TWO".to_owned());
+
+            assert!(under_test.rollback("ONE".to_owned()));
+            for (k, v) in &epochs[0] {
+                assert_eq!(Some(v), under_test.get(k));
+            }
+
+            assert!(under_test.rollback("TWO".to_owned()));
+            for (k, _v) in &epochs[1] {
+                assert_eq!(None, under_test.get(k));
+            }
+
+            TestResult::passed()
+        }
+        // quickcheck doesn't work with closures, unfortunately
+        QuickCheck::new()
+            .quickcheck(property as fn(HashSet<String>, HashSet<String>) -> TestResult);
+    }
+
+    #[test]
+    fn snapshots_prune() {
+        fn property(keys_one: HashSet<String>) -> TestResult {
+            let entries_one = cartesian_product(keys_one);
+
+            let hashmap = VMapTriv::new();
+            let mut under_test: Box<dyn crate::Mappy<String, u32>> = Box::new(hashmap);
+
+            under_test.checkpoint("EMPTY".to_owned());
+
+            for (key, value) in entries_one.clone().into_iter() {
+                under_test.insert(key, value);
+            }
+
+            under_test.checkpoint("ONE".to_owned());
+
+            under_test.prune();
+            assert!(!under_test.rollback("EMPTY".to_owned()));
+
+            for (k, v) in &entries_one {
+                assert_eq!(Some(v), under_test.get(k))
+            }
+            assert!(!under_test.rollback("ONE".to_owned()));
 
             TestResult::passed()
         }
